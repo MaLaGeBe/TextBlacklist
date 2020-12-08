dceb908bc8b370de74e1fd0b06c206307c2ba161
麻辣戈壁 <lpj.vip@foxmail.com>
2020-12-08 18:22:59 +0800

    Update TextBlacklist.lua

--- TextBlacklist.lua
+++ TextBlacklist.lua
@@ -5,74 +5,67 @@ local http = require("http")
 
 local whitelist = {43604064,366893831} --群白名单
 local blacklist = {
-    "vpn",
-    "VPN",
-    "VPn",
-    "Vpn",
-    "蓝灯",
-    "老王",
-    "科学上网",
-    "翻墙",
-    "快连",
-    "绿灯",
+	"vpn",
+	"VPN",
+	"VPn",
+	"Vpn",
+	"蓝灯",
+	"老王",
+	"科学上网",
+	"翻墙",
+	"快连",
+	"绿灯",
 }
 
 local function contains(table, val)
-    for i = 1, #table do
-       if table[i] == val then
-          return true
-       end
-    end
-    return false
+	for i = 1, #table do
+	if table[i] == val then
+		return 1
+	end
+	end
+	return false
 end
 
 function ReceiveFriendMsg(CurrentQQ, data)
-    return true
+	return 1
 end
 
 function ReceiveGroupMsg(CurrentQQ, data)
-    if not contains(whitelist, data.FromGroupId) then
-        return true
-    end
+	if not contains(whitelist, data.FromGroupId) then
+		return 1
+	end
 
-    if (CurrentQQ == data.FromUserId) then
-        return true
-    end
+	if data.FromUserId == 993846145 then
+		return 1
+	end
 
-    for i = 1, #blacklist do
-        if (string.match(data.Content, blacklist[i]) == blacklist[i]) then
-            LuaRes =
-            Api.Api_SendMsg(
-            CurrentQQ,
-            {
-                toUser = data.FromGroupId,
-                sendToType = 2,
-                sendMsgType = "ReplayMsg",
-                groupid = 0,
-                content = "检测到敏感词汇，请撤回！！！",
-                atUser = data.FromUserId,
-                replayInfo = {
-                    MsgSeq =  data.MsgSeq,
-                    MsgTime = data.MsgRandom,
-                    UserID = data.FromUserId,
-                    RawContent = data.Content
-                }
-            }
-        )
+	for i = 1, #blacklist do
+		if (string.match(data.Content, blacklist[i]) == blacklist[i]) then
+			LuaRes =
+			Api.Api_SendMsgV2(
+				CurrentQQ,
+				{
+					ToUserUid = data.FromGroupId,
+					SendToType = 2,
+					SendMsgType = "TextMsg",
+					Content = "请勿在群内沟通敏感信息[表情14]"
+				}
+			)
 
-        log.notice("From Lua SendMsg Ret-->%d", LuaRes.Ret)
-        -- Api.Api_RevokeMsg(CurrentQQ, data.FromGroupId, data.MsgSeq, data.MsgRandom)
-        -- Api.Api_CallFunc(CurrentQQ, "PbMessageSvc.PbMsgWithDraw", {data.FromGroupId, data.MsgSeq, data.MsgRandom})
-        end
-    end
+			log.notice("From Lua SendMsg Ret-->%d", LuaRes.Ret)
 
-    return true
+			Api.Api_CallFunc(CurrentQQ,"PbMessageSvc.PbMsgWithDraw",{GroupID = data.FromGroupId, MsgSeq = data.MsgSeq, MsgRandom = data.MsgRandom})
+
+		end
+	end
+
+	return 1
 end
 
 function ReceiveEvents(CurrentQQ, data)
-    return true
+	return 1
 end
 
 function GroupMsgParseMsg(CurrentQQ, data)
-    return true
+	return 1
 end
